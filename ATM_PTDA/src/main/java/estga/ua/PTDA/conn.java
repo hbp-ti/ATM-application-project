d4b9d62d8a80fe18d2a3d870ba633d60e439b39d
khuunsh68 <goncalofarias10@ua.pt>
2023-11-24 12:54:17 +0000

    Atualização da class que conecta a BD

--- ATM_PTDA/src/main/java/estga/ua/PTDA/conn.java
+++ ATM_PTDA/src/main/java/estga/ua/PTDA/conn.java
@@ -1,23 +1,41 @@
 package estga.ua.PTDA;
+
 import java.sql.*;
+import java.util.Scanner;
 
 public class conn {
     public static void main(String[] args) {
-        conn con = new conn();
-        con.connect();
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("URL da base de dados: ");
+        String url = sc.nextLine();
+
+        System.out.println("Schema da base de dados: ");
+        String schema = sc.nextLine();
+
+        System.out.println("Nome de utilizador: ");
+        String user = sc.nextLine();
+
+        System.out.println("Passwoerd: ");
+        String pass = sc.nextLine();
+
+        try {
+            conn connection = new conn();
+            connection.connect(url, schema, user, pass);
+        } catch(Exception e) {
+            System.out.println("Erro ao conectar: " + e.getMessage());
+        }
     }
-    public void connect() {
-        String url = "jdbc:mysql://estga-dev.ua.pt:3306/PTDA_BD_003";
-        String user = "PTDA_003";
-        String password = "Gos_493ft";
+    public void connect(String url, String schema, String user, String password) {
         Connection c;
         Statement s;
 
         try  {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            c = DriverManager.getConnection(url, user, password);
-            System.out.println("Conexão bem-sucedida!");
+            c = DriverManager.getConnection("jdbc:mysql://" + url + ":3306/" + schema + "", user, password);
             s = c.createStatement();
+            System.out.println("Conexão bem-sucedida!");
         } catch (SQLException | ClassNotFoundException e) {
             System.err.println("Erro de conexão: " + e.getMessage());
         }
