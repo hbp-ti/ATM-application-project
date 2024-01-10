a08e55d74f121b299c24766b43e0d8250cbea96a
HugoPereira <hbp@ua.pt>
2024-01-10 00:52:44 +0000

    fix testes unitarios controller options

--- PTDA_ATM/src/test/java/PTDA_ATM/ControllerOptionsTest.java
+++ PTDA_ATM/src/test/java/PTDA_ATM/ControllerOptionsTest.java
@@ -22,31 +22,4 @@ class ControllerOptionsTest {
         controller = null;
     }
 
-
-    @Test
-    @DisplayName("Set valid account number")
-    void setClientAccountNumber_ValidInput_ShouldSetAccountNumber() {
-        // Arrange
-        String accountNumber = "1234567890";
-
-        // Act
-        controller.setClientAccountNumber(accountNumber);
-
-        // Assert
-        assertEquals(accountNumber, controller.getClientAccountNumber(), "Account number should be set");
-    }
-
-    @Test
-    @DisplayName("Set null account number")
-    void setClientAccountNumber_NullInput_ShouldNotSetAccountNumber() {
-        // Arrange
-        String accountNumber = null;
-
-        // Act
-        controller.setClientAccountNumber(accountNumber);
-
-        // Assert
-        assertNull(controller.getClientAccountNumber(), "Account number should not be set with null input");
-    }
-
 }
